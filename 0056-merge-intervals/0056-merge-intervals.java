95ad8b5d2a671af846e908570acdb24621f81f44
ANUPRIYA-TIWARI <88017965+ANUPRIYA-TIWARI@users.noreply.github.com>
2023-05-11 20:45:43 +0530

    Time: 12 ms (15.03%), Space: 47.5 MB (51.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,7 +3,6 @@ class Solution {
     List<int[]> ans = new ArrayList<>();
 
     Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
-
     for (int[] interval : intervals)
       if (ans.isEmpty() || ans.get(ans.size() - 1)[1] < interval[0])
         ans.add(interval);
